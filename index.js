8d7e1251360cb7372718cad0f4e846bebe373d3c
Mariam <mariam_khakimova@mail.ru>
2021-07-21 14:17:18 +0300

    create calculator

---
+++ index.js
@@ -0,0 +1,47 @@
+// Сложение
+function add() {
+    let number1 = document.getElementById("number1").value;
+    number1 = parseInt(number1);
+
+    let number2 = document.getElementById("number2").value;
+    number2 = parseInt(number2);
+
+    let result = (Number(number1) + Number(number2));
+    document.getElementById("out").innerHTML = result;
+}
+
+// Вычитание
+function subtract() {
+    let number1 = document.getElementById("number1").value;
+    number1 = parseInt(number1);
+
+    let number2 = document.getElementById("number2").value;
+    number2 = parseInt(number2);
+
+    const result = (Number(number1) - Number(number2));
+    document.getElementById("out").innerHTML = result;
+}
+
+// Умножение
+function multiply() {
+    let number1 = document.getElementById("number1").value;
+    number1 = parseInt(number1);
+
+    let number2 = document.getElementById("number2").value;
+    number2 = parseInt(number2);
+
+    const result = (Number(number1) * Number(number2));
+    document.getElementById("out").innerHTML = result;
+}
+
+// Деление
+function divide() {
+    let number1 = document.getElementById("number1").value;
+    number1 = parseInt(number1);
+
+    let number2 = document.getElementById("number2").value;
+    number2 = parseInt(number2);
+
+    const result = (Number(number1) / Number(number2));
+    document.getElementById("out").innerHTML = result;
+}
